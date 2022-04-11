02bce5837b1ac56fa0e7f46f7ab180d5cb543af5
PawelM88 <mlynarczyk-p@wp.pl>
2022-04-11 17:39:25 +0200

    some minor name fixes

--- app/Http/Controllers/FuelsController.php
+++ app/Http/Controllers/FuelsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// This controller exists only for the Main Site
 class FuelsController extends Controller
 {
     public function index()
